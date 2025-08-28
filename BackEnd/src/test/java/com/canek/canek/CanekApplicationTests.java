f599a1e77cca9e7f6d78827a9133fc314cc694df
Felipe Anthony <felipe.canalcanal@gmail.com>
2025-08-27 22:42:08 -0300

    first commit

---
+++ BackEnd/src/test/java/com/canek/canek/CanekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canek.canek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
